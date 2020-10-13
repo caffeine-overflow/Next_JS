d2c77e4382270ab6e7be1eb696f00e466a97a77b
popcornn95 <danishdavis@hotmail.com>
2020-10-12 21:51:11 -0400

    added skills section

--- components/pageIntro.js
+++ components/pageIntro.js
@@ -33,12 +33,12 @@ function PageIntro() {
                         <CharacterReveal
                             animateOpacity
                             canPlay={fire}
-                            characterOffsetDelay={15}
+                            characterOffsetDelay={10}
                             characterWordSpacing={'.25em'}
                             copy={[
                                 'I\'m a Computer programmer offering a strong foundation in software engineering and programming principles. Experience in object-oriented programming; developing, testing and debugging code; designing interfaces; and managing database systems. Bringing strong work ethic and excellent organizational skills to any setting. Excited to begin a new challenge with a successful team.'
                             ]}
-                            duration={1500}
+                            duration={500}
                             ease={
                                 'cubic-bezier(0.5848375451263538,-0.003374999999999906,0.16606498194945848,1.012625)'
                             }
--- components/skills.js
+++ components/skills.js
@@ -12,34 +12,60 @@ function PageIntro() {
     const [first, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
     const [second, setSecond] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
 
+    function onChange(isVisible) {
+        if (isVisible) {
+            document.getElementById('skillsImage1').className = "card cardImage";
+        }
+        else {
+            document.getElementById('skillsImage1').className = "card";
+        }
+    }
+
+    function onChange2(isVisible) {
+        if (isVisible) {
+            document.getElementById('skillsImage2').className = "card cardImage2";
+        }
+        else {
+            document.getElementById('skillsImage2').className = "card";
+        }
+    }
+
     return (
         <div className="skills">
-            <div style={{ width: '100%' }}>
+            <div style={{ width: '90%', margin: 'auto' }}>
                 <div className="skillsHeader">What I'm familiar with?</div>
                 <div className="skillsContainer">
-                    <animated.div
-                        className="card"
-                        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-                        onMouseLeave={() => set({ xys: [0, 0, 1] })}
-                        style={{ transform: first.xys.interpolate(trans) }}
-                    >
-                        <img style={{ width: '100%' }} src={require("../images/main/skills.png")} alt="skills1" />
-                    </animated.div>
-                    <animated.div
-                        className="card"
-                        onMouseMove={({ clientX: x, clientY: y }) => setSecond({ xys: calc(x, y) })}
-                        onMouseLeave={() => setSecond({ xys: [0, 0, 1] })}
-                        style={{ transform: second.xys.interpolate(trans) }}
-                    >
-                        <img style={{ width: '100%' }} src={require("../images/main/skills2.png")} alt="skills1" />
-                    </animated.div>
+                    <VisibilitySensor onChange={onChange} partialVisibility={true}>
+                        <div>
+                            <animated.div
+                                id="skillsImage1"
+                                className="card"
+                                onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+                                onMouseLeave={() => set({ xys: [0, 0, 1] })}
+                                style={{ transform: first.xys.interpolate(trans) }}
+                            >
+                                <img style={{ width: '100%' }} src={require("../images/main/skills.png")} alt="skills1" />
+                            </animated.div>
+                        </div>
+                    </VisibilitySensor>
+                    <VisibilitySensor onChange={onChange2} partialVisibility={true}>
+                        <animated.div
+                            id="skillsImage2"
+                            className="card"
+                            onMouseMove={({ clientX: x, clientY: y }) => setSecond({ xys: calc(x, y) })}
+                            onMouseLeave={() => setSecond({ xys: [0, 0, 1] })}
+                            style={{ transform: second.xys.interpolate(trans) }}
+                        >
+                            <img style={{ width: '100%' }} src={require("../images/main/skills2.png")} alt="skills1" />
+                        </animated.div>
+                    </VisibilitySensor>
                 </div>
             </div>
 
             <style jsx>{`
                 .skills{
                     overflow: hidden;
-                    height:2000px   ;
+                    height:1500px;
                     margin: 50px auto 0 auto;
                 }
                 .skillsHeader{
@@ -48,9 +74,6 @@ function PageIntro() {
                     color:#161748;
                     text-align: center;
                     margin-bottom: 60px;
-                }
-                .skillsContainer{
-                  
                 }
             `}</style>
         </div>
