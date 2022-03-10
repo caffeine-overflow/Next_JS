4844f12cf5cd8dfde432025d24517937f377862c
popcornn95 <danishdavis@hotmail.com>
2022-03-09 22:13:49 -0500

    added minimum width

--- pages/_app.js
+++ pages/_app.js
@@ -6,13 +6,14 @@ import React, { useState, useEffect } from 'react';
 function MyApp({ Component, pageProps }) {
   const [flag, setFlag] = useState(false);
   useEffect(() => {
-    console.log(window.innerWidth)
-    if (window.innerWidth < 1150) setFlag(true);
+    if (window.innerWidth < 1280) setFlag(true);
   }, []);
 
   return <AnimateSharedLayout exitBeforeEnter>
     {
-      flag ? <div>Mobile view is not available yet!</div> : <Component {...pageProps} />
+      flag ?
+        <div>Mobile view is not available yet!</div> :
+        <div style={{ minWidth: '1280px' }}><Component {...pageProps} /></div>
     }
   </AnimateSharedLayout>
 }
